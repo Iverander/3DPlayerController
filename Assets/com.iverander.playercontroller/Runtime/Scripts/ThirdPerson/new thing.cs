c65d99a74fb834cb5a1d003391b81a922ccafd13
Iverander <torje@iverander.com>
2026-05-22 22:27:52 +0200

    fix

---
+++ Assets/com.iverander.playercontroller/Runtime/Scripts/ThirdPerson/new thing.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Iverander
+{
+    public class newthing : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
